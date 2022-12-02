e313bbadcf0254675195c3cec58324676f10d724
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-12-02 17:25:37 -0300

    Reinventing the wheel. Again

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// DEPENDENCY
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// COMPONENT
+import App from './App'
+
+// STYLE
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
